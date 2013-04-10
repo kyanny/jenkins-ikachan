eff180bc12643b2b5506d3e6965fa38dfe11920b
Kensuke Nagae <kyanny@gmail.com>
2013-04-10 11:53:56 +0900

    debug

--- ikachan.sh
+++ ikachan.sh
@@ -3,5 +3,6 @@
 set -x
 
 curl -s -F channel=\\$channel $join_url >/dev/null 2>&1
-result=`curl -s ${url}api/xml | perl colorize.pl`
+xml=`curl -s ${url}api/xml`
+result=`curl -s https://gist.github.com/kyanny/cfe1ba03a72f86a18477/raw/colorize.pl | perl < $xml`
 curl -s -F channel=\\$channel -F message="Jenkins ($job): $result - $url $notice_url
