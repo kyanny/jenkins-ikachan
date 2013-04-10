b4a7e00e74ab1babc79c416ce5c554b1ccac115c
Kensuke Nagae <kyanny@gmail.com>
2013-04-10 11:24:06 +0900

    silent

--- ikachan.sh
+++ ikachan.sh
@@ -2,6 +2,6 @@
 
 set -x
 
-curl -s -F channel=\\$channel $join_url
+curl -s -F channel=\\$channel $join_url >/dev/null 2>&1
 result=`curl -s ${build_url}api/xml | perl -le '$_=<>;/<result>(.+?)</;printf "\x02\x0301,%02d%s\x0f",{A=>8,S=>3,F=>4}->{substr $1,0,1},$1'`
 curl -s -F channel=\\$channel -F message="Jenkins ($job_name): $result - $build_url $notice_url
